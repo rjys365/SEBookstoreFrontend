59b7776a9b44d9e16a19cd060750db8974a77979
rjys365 <rjys365@hotmail.com>
2023-04-23 08:34:07 +0800

    Order digests page

---
+++ src/util/OrderDigest.js
@@ -0,0 +1,8 @@
+export class OrderDigest{
+    constructor(id,itemCount,totalPrice,firstItemTitle,){
+        this.id=id;
+        this.itemCount=itemCount;
+        this.totalPrice=totalPrice;
+        this.firstItemTitle=firstItemTitle;
+    }
+};
--- src/view/Orders.js
+++ src/view/Orders.js
@@ -1,13 +1,50 @@
-import { Empty } from "antd";
-import React, { useContext } from "react";
+import { Empty, List } from "antd";
+import React, { useContext, useEffect, useState } from "react";
 import { LoginContext } from "../service/LoginContext";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import { OrderDigest } from "../util/OrderDigest";
+
+const ORDERS=[
+    new OrderDigest(1,2,100,"book1"),
+];
+
 export function Orders(){
     const login=useContext(LoginContext);
-    if(!login.token)return <Navigate to='/login'/>;
+    const [orders,setOrders]=useState(null);
+    const loadOrders=()=>{
+        const load=async ()=>{
+            const response=await fetch('http://localhost:8080/orders/allOrders');
+            const json=await response.json();
+            setOrders(json);
+        };
+        load();
+    }
+    useEffect(()=>{loadOrders()},[]);
+    if(!login.token)return <Navigate to={'/login?back='+encodeURIComponent('/orders')}/>;
+    if(orders===null)return (
+        <div>
+            <Empty description="订单信息加载中"/>
+        </div>
+    )
     return (
         <div>
-            <Empty description="暂无订单"/>
+            <h1>订单列表</h1>
+            <List
+                itemLayout="horizontal"
+                dataSource={orders}
+                renderItem={item=>(
+                    <List.Item key={'order-'+item.id}>
+                        <List.Item.Meta
+                            title={<div><span>订单号: </span><span><Link to={'/orders/'+item.id}>{item.id}</Link></span></div>}
+                            description={<div>
+                                <div><span>订单总价: ￥</span><span>{item.totalPrice}</span></div>
+                                <div>{item.firstItemTitle+'等'+item.itemCount+'件商品'}</div>
+                            </div>}
+                        />
+                    </List.Item>
+                )}
+            />
         </div>
+
     )
 }
