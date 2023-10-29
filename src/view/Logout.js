dacffd259c3b98b89773df5970fea2a66cdfa932
Zhang Zhuyue <zhang.zhuyue@sjtu.edu.cn>
2023-10-29 15:42:15 +0800

    fix logout

--- src/view/Logout.js
+++ src/view/Logout.js
@@ -1,11 +1,13 @@
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import {Button, Spin} from "antd";
+import {LoginContext} from "../service/LoginContext";
 
 export function Logout() {
-    const [loggingOut,setLoggingOut]=useState(false);
+    const [loggingOut, setLoggingOut] = useState(false);
     const [logoutDto, setLogoutDto] = useState(undefined);
+    const loginContext = useContext(LoginContext);
     useEffect(() => {
-        if (logoutDto === undefined&&loggingOut) {
+        if (logoutDto === undefined && loggingOut) {
             fetch('http://localhost:8080/login/', {
                 method: 'DELETE',
                 credentials: 'include'
@@ -14,7 +16,7 @@ export function Logout() {
                     if (response.ok) return response.json();
                     else localStorage.removeItem('login');
                 },
-                ()=>{
+                () => {
                     setLogoutDto(null);
                 }
             ).then((json) => {
@@ -22,15 +24,19 @@ export function Logout() {
                 setLogoutDto(json);
             });
         }
-    }, [loggingOut,logoutDto]);
+    }, [loggingOut, logoutDto]);
     useEffect(() => {
         // alert(JSON.stringify(logoutDto));
         if (logoutDto !== undefined) {
             localStorage.removeItem('login');
+            if (loginContext) loginContext.setLogin(null);
         }
     }, [logoutDto]);
 
-    if (logoutDto === undefined) return <Button onClick={()=>{setLoggingOut(true)}}>确认注销</Button>;
+    if (loggingOut && logoutDto === undefined) return <Spin size='large'/>
+    if (logoutDto === undefined) return <Button onClick={() => {
+        setLoggingOut(true)
+    }}>确认注销</Button>;
     if (!logoutDto) return <div>注销失败</div>;
     return <div>{"注销成功，距离登录毫秒数" + logoutDto.millisSinceLogin}</div>;
 }
